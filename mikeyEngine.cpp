81c4edf173444711c8511f696bd584556efee804
Michael Chang <mchang0926@gmail.com>
2019-12-01 15:41:18 -0800

    can compile stuff from glew  library

--- mikeyEngine.cpp
+++ mikeyEngine.cpp
@@ -8,17 +8,26 @@ int main()
     Window window("Mikey!", 960, 540);
     glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
+    // std::cout << "working" << std::endl;
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    // std::cout << "working" << std::endl;
 
     while (!window.closed())
     {
         // std::cout << window.getWidth() << ", " << window.getHeight() << std::endl;
         window.clear();
-        glBegin(GL_TRIANGLES);
+#if 1
+        glBegin(GL_QUADS);
         glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
+        glVertex2f(-0.5f,  0.5f);
+        glVertex2f( 0.5f,  0.5f);
         glVertex2f( 0.5f, -0.5f);
         glEnd();
+#else
+        glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+#endif
         window.update();
     }
 
